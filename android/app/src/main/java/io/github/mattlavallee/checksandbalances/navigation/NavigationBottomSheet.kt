b6e5cadaf869511da909b766464e1b5ca7965367
Matt Lavallee <matthew.t.lavallee@gmail.com>
2019-12-30 11:31:43 -0500

    feat(app): updating drawer navigation menus
    Adding navigation menu for navigation icon in app bar
    Also customizing color and shape of menu items in the drawer menus

--- android/app/src/main/java/io/github/mattlavallee/checksandbalances/navigation/NavigationBottomSheet.kt
+++ android/app/src/main/java/io/github/mattlavallee/checksandbalances/navigation/NavigationBottomSheet.kt
@@ -1,14 +1,35 @@
 package io.github.mattlavallee.checksandbalances.navigation
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.navigation.NavigationView
 import io.github.mattlavallee.checksandbalances.R
 
 class NavigationBottomSheet: BottomSheetDialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.layout_home_bottomsheet, container, false)
+        val homeView = inflater.inflate(R.layout.layout_home_bottomsheet, container, false)
+
+        val navigationView: NavigationView = homeView.findViewById(R.id.home_navigation_view)
+        navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home_create_account_menu_button -> toast(activity!!.applicationContext, "Create Account")
+                R.id.home_create_transaction_menu_button -> toast(activity!!.applicationContext, "Create Transaction")
+                R.id.home_help_feedback_menu_button -> toast(activity!!.applicationContext, "Help me Rhonda")
+            }
+
+            super.dismiss()
+            true
+        }
+
+        return homeView
+    }
+
+    private fun toast(context: Context, message: String) {
+        return Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 }
