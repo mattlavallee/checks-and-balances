a3605914b07ebe6a8de58c4104172fef2c9de9f9
Matt Lavallee <matthew.t.lavallee@gmail.com>
2023-01-02 15:42:55 -0500

    refactor(options): cleaning up navigation bottomsheet code

--- android/app/src/main/java/io/github/mattlavallee/checksandbalances/ui/navigation/NavigationBottomSheet.kt
+++ android/app/src/main/java/io/github/mattlavallee/checksandbalances/ui/navigation/NavigationBottomSheet.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import io.github.mattlavallee.checksandbalances.R
 import io.github.mattlavallee.checksandbalances.core.Constants
@@ -16,20 +17,14 @@ class NavigationBottomSheet: BottomSheetDialogFragment() {
         val homeView = inflater.inflate(R.layout.layout_home_bottomsheet, container, false)
         binding = LayoutHomeBottomsheetBinding.bind(homeView)
 
-        val fragManager = requireActivity().supportFragmentManager
-        val stackCount = fragManager.backStackEntryCount
-        val currStackEntry = if (stackCount > 0) fragManager.getBackStackEntryAt(stackCount - 1) else null
-        val activeFragment = fragManager.findFragmentByTag(currStackEntry?.name)
-        val isTransactionFragment = currStackEntry?.name == Constants.transactionViewTag
-        val deleteTransOpt = binding.homeNavigationView.menu.getItem(2)
-        deleteTransOpt.isEnabled = isTransactionFragment
+        val transFragment = this.setArchiveTransactionState(requireActivity().supportFragmentManager)
 
         binding.homeNavigationView.setNavigationItemSelectedListener {
             val fragmentManager = requireActivity().supportFragmentManager
             when (it.itemId) {
                 R.id.home_create_account_menu_button -> FormDispatcher.launch(fragmentManager, Constants.accountFormTag, null)
                 R.id.home_create_transaction_menu_button -> FormDispatcher.launch(fragmentManager, Constants.transactionFormTag, null)
-                R.id.home_delete_all_transactions_menu_button -> (activeFragment as TransactionFragment).deleteTransactions()
+                R.id.home_delete_all_transactions_menu_button -> transFragment?.deleteTransactions()
                 R.id.home_help_feedback_menu_button -> FormDispatcher.launch(fragmentManager, Constants.feedbackTag, null)
             }
 
@@ -38,5 +33,17 @@ class NavigationBottomSheet: BottomSheetDialogFragment() {
         }
 
         return homeView
+    }
+
+    private fun setArchiveTransactionState(fragManager: FragmentManager): TransactionFragment? {
+        val stackCount = fragManager.backStackEntryCount
+        val currStackEntry = if (stackCount > 0) fragManager.getBackStackEntryAt(stackCount - 1) else null
+
+        val isTransactionFragment = currStackEntry?.name == Constants.transactionViewTag
+        val deleteTransOpt = binding.homeNavigationView.menu.getItem(2)
+        deleteTransOpt.isEnabled = isTransactionFragment
+
+        val activeFragment = fragManager.findFragmentByTag(currStackEntry?.name)
+        return if (activeFragment == null) null else activeFragment as TransactionFragment
     }
 }
